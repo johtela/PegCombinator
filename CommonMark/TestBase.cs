e7eb45c036e3f5d8d11959e414797bcdd7a45086
johtela <tommi.johtela@dnainternet.net>
2018-06-28 15:02:50 +0300

    Did some initial benchmarking agains Markdig.

--- CommonMark/TestBase.cs
+++ CommonMark/TestBase.cs
@@ -1,10 +1,30 @@
-﻿namespace CommonMark
+﻿//#define markdig
+
+namespace CommonMark
 {
-	using MarkdownPeg;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+#if markdig
+	using Markdig;
+#else
+	using MarkdownPeg;
+#endif
 
 	public class TestBase
 	{
+#if markdig
+		private static MarkdownPipeline _pipeline;
+
+		static TestBase ()
+		{
+			_pipeline = new MarkdownPipelineBuilder ().Build ();
+		}
+
+		protected static void TestParse (string input, string output)
+		{
+			var parsed = Markdown.ToHtml (input, _pipeline);
+			Assert.AreEqual (output.Trim (), parsed.Trim ());
+		}
+#else
 		public static MarkdownToHtml Parser;
 
 		static TestBase ()
@@ -17,6 +37,6 @@
 			var parsed = Parser.Run (input);
 			Assert.AreEqual (output.Trim (), parsed.Trim ());
 		}
-
+#endif
 	}
 }
